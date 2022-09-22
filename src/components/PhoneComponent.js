4b4676b9126073d3897901bbf5d37ff18a450f90
cem-sucu <cems.cems.17@gmail.com>
2022-09-22 10:07:03 +0200

    ation et reducer

---
+++ src/components/PhoneComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function PhoneComponent() {
+    return (
+        <div className="container">
+            <img src="" alt="phone" />
+            <p>
+                Disponibilité
+                <span id="count"></span>
+            </p>
+            <button>Acheter</button>
+        </div>
+    );
+}
+
+export default PhoneComponent;
---
+++ src/redux/phone/actionPhone.js
@@ -0,0 +1,7 @@
+import { BUY_PHONE } from "./type";
+
+const buyPhone = () => {
+    return {
+        type: BUY_PHONE,
+    };
+};
