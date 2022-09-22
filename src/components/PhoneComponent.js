6230cc725909f0f3396bddfc14ea5ce6f1361152
cem-sucu <cems.cems.17@gmail.com>
2022-09-22 14:06:14 +0200

    useSelector Hook

--- src/components/PhoneComponent.js
+++ src/components/PhoneComponent.js
@@ -1,4 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { buyPhone } from "../redux/phone/actionPhone";
+
+function PhoneComponent(props) {
+    const phones = useSelector((state) => state.phones);
+
+    return (
+        <div className="container">
+            <img src="" alt="phone" />
+            <p>
+                Disponibilité :<span id="count"> {phones}</span>
+            </p>
+            <button>Acheter</button>
+        </div>
+    );
+}
+
+const mapDispatchToprops = (dispatch) => {
+    return {
+        buyPhone: () => dispatch(buyPhone()),
+    };
+};
+
+export default PhoneComponent;
+
+/* import React from "react";
 import { connect } from "react-redux";
 import { buyPhone } from "../redux/phone/actionPhone";
 
@@ -28,3 +54,4 @@ const mapDispatchToprops = (dispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToprops)(PhoneComponent);
+ */
